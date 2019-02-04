0b008239d28efd77b207d05abde90c9b78a55fc9
Goran Djuric <gdjuric1989@gmail.com>
2019-02-04 22:07:45 +0100

    two function more!!!

--- main_2.js
+++ main_2.js
@@ -1,10 +1,27 @@
 window.addEventListener('load', () => {
-	let options = ['7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm'];
-	let select = document.querySelector('#select_time');
+	let options_time = ['7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm'];
+	
+	let option_person = ['1 person', '2 persons', '3 persons', '4 person', '5 persons', '6 persons'];
 
-	options.forEach(x => {
-		let item = document.createElement('OPTION');
-		select.appendChild(item);
-		item.innerHTML += x;
+	let select_p_time = document.querySelectorAll('.select_time');
+
+	select_p_size = document.querySelectorAll('.party_size');
+
+	select_p_time.forEach(x => {
+		options_time.forEach(y => {
+			let item = document.createElement('OPTION');
+			x.appendChild(item);
+			item.innerHTML += y;
+
+		});
 	});
+
+	select_p_size.forEach(z => {
+		option_person.forEach(w => {
+			let item = document.createElement('OPTION');
+			z.appendChild(item);
+			item.innerHTML += w;
+		});
+	});
+
 });
